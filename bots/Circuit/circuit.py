1c45ded3519fa6890c4f14a3c2159c990bd7d0a8
8 Day <keyboardwars@riseup.net>
2020-12-10 03:18:40 -0500

    Create circuit.py

---
+++ bots/Circuit/circuit.py
@@ -0,0 +1,10 @@
+# Circuit self bot written on a chromebook with slightly less..
+# ..laziness than usual? Rip or claim this program all you want.
+# Notice it's a simple program written by I(8 Day). That you can..
+# Complete in your sleep with just abit of try....
+# Anyways cya nerds
+try:
+	import discord, time, colorama
+except ImportError as Err:
+	print(f"Module not found: {Err.name}, so install it nerd.")
+
