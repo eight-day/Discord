63188a522f3b213b483d6f1c2b2734ec578e384d
8 Day <keyboardwars@riseup.net>
2020-12-14 06:32:58 -0500

    Update circuit.py

--- bots/Circuit/circuit.py
+++ bots/Circuit/circuit.py
@@ -99,11 +99,11 @@ async def on_message(message: Message) -> None:
 				}
 				await message.delete()
 				async for msg in channel.history(limit=None):
-					if isinstance(msg, MessageType.default):
+					if isinstance(type(msg), MessageType.default):
 						arguments["outdata"].append(
 							f"[{msg.creation_date}] {msg.author} - {msg.content}"
 						)
-					if isinstance(msg, MessageType.call):
+					if isinstance(type(msg), MessageType.call):
 						arguments["outdata"].append(
 							f"[{msg.creation_data}] {msg.author} - Called."
 						)
